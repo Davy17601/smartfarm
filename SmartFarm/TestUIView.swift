2be628c90caa9e7177a0378e4af975c1ec29b890
Davy17601 <davy61540@gmail.com>
2026-05-14 19:32:32 -0700

    - Update test view

--- SmartFarm/TestUIView.swift
+++ SmartFarm/TestUIView.swift
@@ -9,12 +9,8 @@ import SwiftUI
 
 struct TestUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World! Davy")
     }
 }
 
-struct TestUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        TestUIView()
-    }
-}
+

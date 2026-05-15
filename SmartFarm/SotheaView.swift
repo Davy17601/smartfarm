fd4cc75fe1365114aabb2b1096ac27bd31f9fd54
Sothea <nemsothea13@gmail.com>
2026-05-15 09:35:36 +0700

    Create new Sotheaview

---
+++ SmartFarm/SotheaView.swift
@@ -0,0 +1,18 @@
+//
+//  SotheaView.swift
+//  SmartFarm
+//
+//  Created by sothea007 on 15/5/26.
+//
+
+import SwiftUI
+
+struct SotheaView: View {
+    var body: some View {
+        Text("Hello, World! Sothea")
+    }
+}
+
+#Preview {
+    SotheaView()
+}
